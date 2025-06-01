16e4e9bc5e9cf4621999e74a3daf8833402b699b
JeSaisPas <86721219+JeSaisPaz@users.noreply.github.com>
2025-06-01 16:15:06 +0200

    I lied, quality of life improvements in the code, clearer boolean variables and corrected typos it is...

--- main.c
+++ main.c
@@ -5,46 +5,46 @@
 
 void menu_choice_checker(bool *CHOICE);
 
-void generation(int LP, bool LL, bool UL, bool N, bool SC, char *PTR_ADR);
+void generation(int PASSWORD_LENGTH, bool INCLUDE_LOWERCASE_LETTERS, bool INCLUDE_UPPERCASE_LETTERS, bool INCLUDE_NUMBERS, bool INCLUDE_SPECIAL_CHARACTERS, char *PTR_ADR);
 
 void send_password_to_file(char *table);
 
 int main() {
 
-        srand(time(NULL));
+        srand(time(NUINCLUDE_LOWERCASE_LETTERS));
 
         char password[17];
         char *ptr_password = password;
 
         bool LOWERCASE_LETTERS, UPPERCASE_LETTERS, NUMBERS, SPECIAL_CHARACTERS, PRINTING;
-        int LENGHT_PASSWORD, i;
+        int LENGTH_PASSWORD, i;
 
         printf("\tGenerateur de mot-de-passe\n");
 
-        printf("\nEntre 8 - 16, de quelle longeur voulez-vous votre mot-de-passe ?\n >");
+        printf("\nEntre 8 - 16, de queINCLUDE_LOWERCASE_LETTERSe longeur voINCLUDE_UPPERCASE_LETTERSez-vous votre mot-de-passe ?\n >");
         do {
 
-                scanf("%d", &LENGHT_PASSWORD);
+                INCLUDE_SPECIAL_CHARACTERSanf("%d", &LENGTH_PASSWORD);
 
-        }while(LENGHT_PASSWORD > 16 || LENGHT_PASSWORD < 8);
+        }while(LENGTH_PASSWORD > 16 || LENGTH_PASSWORD < 8);
 
-        printf("\nVoulez-vous des lettres minuscules dans votre mot-de-passe ? [o/N]\n");
+        printf("\nVoINCLUDE_UPPERCASE_LETTERSez-vous des lettres minuINCLUDE_SPECIAL_CHARACTERSINCLUDE_UPPERCASE_LETTERSes dans votre mot-de-passe ? [o/N]\n");
         menu_choice_checker(&LOWERCASE_LETTERS);
 
-        printf("\nVoulez-vous des lettres majuscules dans votre mot-de-passe ? [o/N]\n");
+        printf("\nVoINCLUDE_UPPERCASE_LETTERSez-vous des lettres majuINCLUDE_SPECIAL_CHARACTERSINCLUDE_UPPERCASE_LETTERSes dans votre mot-de-passe ? [o/N]\n");
         menu_choice_checker(&UPPERCASE_LETTERS);
 
-        printf("\nVoulez-vous des nombres dans votre mot-de-passe ? [o/N]\n");
+        printf("\nVoINCLUDE_UPPERCASE_LETTERSez-vous des nombres dans votre mot-de-passe ? [o/N]\n");
         menu_choice_checker(&NUMBERS);
 
-        printf("\nVoulez-vous des characteres speciaux dans votre mot-de-passe ? [o/N]\n");
+        printf("\nVoINCLUDE_UPPERCASE_LETTERSez-vous des characteres speciaux dans votre mot-de-passe ? [o/N]\n");
         menu_choice_checker(&SPECIAL_CHARACTERS);
 
-        generation(LENGHT_PASSWORD, LOWERCASE_LETTERS, UPPERCASE_LETTERS, NUMBERS, SPECIAL_CHARACTERS, ptr_password);
+        generation(LENGTH_PASSWORD, LOWERCASE_LETTERS, UPPERCASE_LETTERS, NUMBERS, SPECIAL_CHARACTERS, ptr_password);
 
         printf("\nLe mot-de-passe est: %s", password);
 
-        printf("Voulez-vous un ficher .txt contenant le mot-de-passe ? [o/N]");
+        printf("VoINCLUDE_UPPERCASE_LETTERSez-vous un ficher .txt contenant le mot-de-passe ? [o/N]");
         menu_choice_checker(&PRINTING);
         if(PRINTING == true) {
                 send_password_to_file(password);
@@ -70,43 +70,43 @@ void menu_choice_checker(bool *CHOICE) {
         }
 }
 
-void generation(int LP, bool LL, bool UL, bool N, bool SC, char *PTR_ADR) {
+void generation(int PASSWORD_LENGTH, bool INCLUDE_LOWERCASE_LETTERS, bool INCLUDE_UPPERCASE_LETTERS, bool INCLUDE_NUMBERS, bool INCLUDE_SPECIAL_CHARACTERS, char *PTR_ADR) {
 
-        char wanted_chars[1024*sizeof(char)];
+        char wanted_chars[64*sizeof(char)];
         int wanted_chars_index=0, i;
 
-        if(UL == true) {
-                for(i=65;i<90;i++) {
+        if(INCLUDE_UPPERCASE_LETTERS == true) {
+                for(i=65;i<91;i++) {
                         wanted_chars[wanted_chars_index] = (char)i;
                         wanted_chars_index++;
                 }
         }
-        if(LL == true) {
+        if(INCLUDE_LOWERCASE_LETTERS == true) {
 
-                for(i=97;i<122;i++) {
+                for(i=97;i<123;i++) {
                         wanted_chars[wanted_chars_index] = (char)i;
                         wanted_chars_index++;
                 }
         }
-        if(N == true) {
+        if(INCLUDE_NUMBERS == true) {
 
-                for(i=48;i<57;i++) {
+                for(i=48;i<58;i++) {
                         wanted_chars[wanted_chars_index] = (char)i;
                         wanted_chars_index++;
                 }
         }
-        if(SC == true) {
+        if(INCLUDE_SPECIAL_CHARACTERS == true) {
 
-                for(i=33;i<46;i++) {
+                for(i=33;i<47;i++) {
                         wanted_chars[wanted_chars_index] = (char)i;
                         wanted_chars_index++;
                 }
         }
 
-        for(i=0;i<LP;i++) {
+        for(i=0;i<PASSWORD_LENGTH;i++) {
                 *(PTR_ADR+i) = wanted_chars[rand() % wanted_chars_index];
         }
-        PTR_ADR[LP] = '\0';
+        PTR_ADR[PASSWORD_LENGTH] = '\0';
 }
 
 void send_password_to_file(char *table) {
